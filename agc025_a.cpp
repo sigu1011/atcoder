6c35e4d47266f87ee1b667b60a1c9ab8a784c694
sigu <52273288+sigu1011@users.noreply.github.com>
2019-07-19 20:23:21 +0900

    update

--- agc025_a.cpp
+++ agc025_a.cpp
@@ -4,35 +4,19 @@ using namespace std;
 int main() {
   int N;
   scanf("%d", &N);
-  int A = N / 2;
-  int B = N - A;
 
-  int min_sum_all = 100000;
-  for (int i = 0; i < N / 2; i++) {
-    int sum_A = 0;
-    int sum_B = 0;
-
-    int tmp_A = A;
-    while (tmp_A > 0) {
-      int a = tmp_A % 10;
-      sum_A += a;
-      tmp_A = tmp_A / 10;
-    }
-
-    int tmp_B = B;
-    while (tmp_B > 0) {
-      int b = tmp_B % 10;
-      sum_B += b;
-      tmp_B = tmp_B / 10;
-    }
-
-    if (sum_A + sum_B < min_sum_all) {
-      min_sum_all = sum_A + sum_B;
-    }
-
-    A--;
-    B++;
+  int sum = 0;
+  int tmp = N;
+  while (tmp > 0) {
+    int t = tmp % 10;
+    sum += t;
+    tmp = tmp / 10;
   }
-  printf("%d", min_sum_all);
+
+  if (sum == 1)
+    printf("10");
+  else
+    printf("%d", sum);
+
   return 0;
 }
