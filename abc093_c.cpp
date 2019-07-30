52476abebdc9f22a2d67d997e86e2e4fc8145f2c
sigu <52273288+sigu1011@users.noreply.github.com>
2019-07-30 16:32:42 +0900

    update

--- abc093_c.cpp
+++ abc093_c.cpp
@@ -5,33 +5,13 @@ int main() {
   int A, B, C;
   scanf("%d%d%d", &A, &B, &C);
 
-  int array[3];
-  int array_len = sizeof(array) / sizeof(array[0]);
-  sort(array, array + array_len);
-
-  int is_even_count = 0;
-  for (auto i : array) {
-    if (i % 2 == 0) is_even_count++;
-  }
+  int M = max(max(A, B), C);
 
   int ans = 0;
-  if (is_even_count == 3 || is_even_count == 0) {
-    ans += (array[2] - array[0]) / 2;
-    ans += (array[2] - array[1]) / 2;
-  } else if (is_even_count == 2) {
-    for (int i = 0; i < 3; i++) {
-      if (array[i] % 2 == 0) array[i]++;
-    }
-    ans++;
-    ans += (array[2] - array[0]) / 2;
-    ans += (array[2] - array[1]) / 2;
-  } else if (is_even_count == 1) {
-    for (int i = 0; i < 3; i++) {
-      if (array[i] % 2 != 0) array[i]++;
-    }
-    ans++;
-    ans += (array[2] - array[0]) / 2;
-    ans += (array[2] - array[1]) / 2;
+  if ((3 * M - A - B - C) % 2 == 0) {
+    ans = (3 * M - A - B - C) / 2;
+  } else {
+    ans = (3 * (M + 1) - A - B - C) / 2;
   }
 
   printf("%d", ans);
