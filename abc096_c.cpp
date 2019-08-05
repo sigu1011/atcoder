263f046e27eb2715a23eec882ce867ad00255838
sigu <52273288+sigu1011@users.noreply.github.com>
2019-08-05 14:07:23 +0900

    不要な初期化処理を消去.解法はボード外のマスを定義したもの.

--- abc096_c.cpp
+++ abc096_c.cpp
@@ -6,17 +6,14 @@ int main() {
   scanf("%d%d", &H, &W);
 
   vector<vector<char>> grid(H + 2, vector<char>(W + 2, '0'));
-  for (int i = 0; i < H + 2; i++) {
-    for (int j = 0; j < W + 2; j++) {
-      if (i == 0 || i == H + 1 || j == 0 || j == W + 1) {
-        grid.at(i).at(j) = 'w';
-      } else {
-        char s;
-        cin >> s;
-        grid.at(i).at(j) = s;
-      }
+  for (int i = 1; i <= H; i++) {
+    for (int j = 1; j <= W; j++) {
+      char s;
+      cin >> s;
+      grid.at(i).at(j) = s;
     }
   }
+
   int count = 0;
   for (int i = 1; i <= H; i++) {
     for (int j = 1; j <= W; j++) {
