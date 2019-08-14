ebb16b17c82e726bb040f4475b2289b078711d7c
sigu <52273288+sigu1011@users.noreply.github.com>
2019-08-14 22:49:21 +0900

    右のマスから操作を選ぶことで、処理がより単純になるため変更.

--- abc136_c.cpp
+++ abc136_c.cpp
@@ -10,25 +10,12 @@ int main() {
     scanf("%d", &H.at(i));
   }
 
-  bool is_first = true;
-  int baseline;
-  for (int i = 0; i < N - 1; i++) {
-    if (H.at(i) > H.at(i + 1)) {
-      H.at(i)--;
-      if (is_first) {
-        baseline = H.at(i);
-        is_first = false;
-      } else {
-        if (H.at(i) < baseline) {
-          printf("No");
-          return 0;
-        } else {
-          baseline = H.at(i);
-        }
-      }
+  for (int i = N - 1; i > 0; i--) {
+    if (H.at(i) < H.at(i - 1)) {
+      H.at(i - 1)--;
     }
 
-    if (H.at(i) > H.at(i + 1)) {
+    if (H.at(i) < H.at(i - 1)) {
       printf("No");
       return 0;
     }
