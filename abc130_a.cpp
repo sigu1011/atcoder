d93bb2b898c78626de6194316095edfacfb381e9
sigu <52273288+sigu1011@users.noreply.github.com>
2019-08-18 19:47:27 +0900

    first commit

---
+++ abc130_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int X, A;
+  scanf("%d%d", &X, &A);
+
+  if (X >= A) {
+    printf("10");
+  } else {
+    printf("0");
+  }
+
+  return 0;
+}
