75391c2df488314e6ae754357b315d4d9b94f156
sigu <52273288+sigu1011@users.noreply.github.com>
2019-09-11 13:27:18 +0900

    first commit

---
+++ abc090_a.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  char a[10][10];
+  scanf("%s%s%s", a[0], a[1], a[2]);
+  printf("%c%c%c", a[0][0], a[1][1], a[2][2]);
+  return 0;
+}
