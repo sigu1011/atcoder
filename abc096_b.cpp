d5f4709988954f3db10960517f4b6fd472d3118e
sigu <52273288+sigu1011@users.noreply.github.com>
2019-09-12 16:41:10 +0900

    ループを使用しない解法に変更

--- abc096_b.cpp
+++ abc096_b.cpp
@@ -2,21 +2,8 @@
 using namespace std;
 
 int main() {
-  vector<int> ABC(3, 0);
-  for (int i = 0; i < 3; i++) {
-    scanf("%d", &ABC.at(i));
-  }
-  int K;
-  scanf("%d", &K);
-
-  sort(ABC.begin(), ABC.end());
-
-  int ans = ABC.at(0) + ABC.at(1);
-  for (int i = 0; i < K; i++) {
-    ABC.at(2) = 2 * ABC.at(2);
-  }
-  ans += ABC.at(2);
-
-  printf("%d", ans);
+  int A, B, C, K;
+  scanf("%d%d%d%d", &A, &B, &C, &K);
+  printf("%d", A + B + C + max(C, max(A, B)) * ((1 << K) - 1));
   return 0;
 }
