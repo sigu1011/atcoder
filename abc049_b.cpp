9d5ba270d6ef50f1887206b8d3e9cbb68fe12627
sigu <52273288+sigu1011@users.noreply.github.com>
2019-09-13 16:34:44 +0900

    first commit

---
+++ abc049_b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int H, W;
+  scanf("%d%d", &H, &W);
+
+  string pixel;
+  for (int i = 0; i < H; i++) {
+    cin >> pixel;
+    cout << pixel << endl;
+    cout << pixel << endl;
+  }
+  return 0;
+}
+
