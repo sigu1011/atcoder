5cd54d712215d3e26c75d9520a768fc588fb1947
sigu <52273288+sigu1011@users.noreply.github.com>
2019-09-13 16:09:04 +0900

    first commit

---
+++ abc053_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x;
+  scanf("%d", &x);
+  if (x < 1200) {
+    printf("ABC");
+  } else {
+    printf("ARC");
+  }
+  return 0;
+}
