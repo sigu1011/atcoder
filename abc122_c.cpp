b840b87908d5262d238612c1f22f40d185d7a1d3
sigu <52273288+sigu1011@users.noreply.github.com>
2019-09-15 00:01:17 +0900

    不要なループがあり、修正.累積和を作成せず、単純にACの出現回数を配列で扱うだけで十分.

--- abc122_c.cpp
+++ abc122_c.cpp
@@ -9,18 +9,14 @@ int main() {
 
   vector<int> a(N, 0);
   for (int i = 0; i < N - 1; i++) {
+    a.at(i + 1) = a.at(i);
     if (S.at(i) == 'A' && S.at(i + 1) == 'C') a.at(i + 1)++;
-  }
-
-  vector<int> sum(N + 1, 0);
-  for (int i = 0; i < N; i++) {
-    sum.at(i + 1) = sum.at(i) + a.at(i);
   }
 
   for (int i = 0; i < Q; i++) {
     int left = 0, right = 0;
     scanf("%d%d", &left, &right);
-    printf("%d\n", sum.at(right) - sum.at(left));
+    printf("%d\n", a.at(right - 1) - a.at(left - 1));
   }
   return 0;
 }
