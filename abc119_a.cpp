69f1aa4fcf7db8b42d606f2b3678b63c1ce71539
sigu <52273288+sigu1011@users.noreply.github.com>
2019-09-21 18:42:48 +0900

    first commit

---
+++ abc119_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+
+  if (S <= "2019/04/30") {
+    printf("Heisei");
+  } else {
+    printf("TBD");
+  }
+  return 0;
+}
