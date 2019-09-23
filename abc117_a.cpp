1a9d45b41f9a46e1e20ba9e6d513951287be6f95
sigu <52273288+sigu1011@users.noreply.github.com>
2019-09-23 23:09:31 +0900

    first commit

---
+++ abc117_a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int T, X;
+  scanf("%d%d", &T, &X);
+  double ans = (double)T / (double)X;
+  printf("%.16lf", ans);
+  return 0;
+}
