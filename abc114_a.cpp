ec72cf125f737da71cfa4baea68de3dbc32cf6ab
sigu <52273288+sigu1011@users.noreply.github.com>
2019-09-25 21:13:46 +0900

    first commit

---
+++ abc114_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int X;
+  scanf("%d", &X);
+
+  if (X == 7 || X == 5 || X == 3)
+    printf("YES");
+  else
+    printf("NO");
+
+  return 0;
+}
