14be3172aeda804f7c640e138d9685f32e7f5298
sigu <52273288+sigu1011@users.noreply.github.com>
2019-10-09 23:31:02 +0900

    first commit

---
+++ abc104_a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int R;
+  scanf("%d", &R);
+
+  if (R < 1200)
+    printf("ABC");
+  else if (1200 < R && R < 2800)
+    printf("ARC");
+  else {
+    printf("AGC");
+  }
+  return 0;
+}
