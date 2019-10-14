0f669231a460e9d3fbd583c612b7abad3b64cbf0
sigu <52273288+sigu1011@users.noreply.github.com>
2019-10-14 23:53:45 +0900

    first commit

---
+++ abc100_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  scanf("%d%d", &A, &B);
+  if (A <= 8 && B <= 8) {
+    printf("Yay!");
+  } else {
+    printf(":(");
+  }
+  return 0;
+}
