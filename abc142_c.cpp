724c54873a4b8b600c9bd1751cb84401cbc9241c
sigu <52273288+sigu1011@users.noreply.github.com>
2019-10-19 20:23:05 +0900

    sortを使用しない解法に変更.ソート後のインデックスを求める場合は配列が2つあれば十分

--- abc142_c.cpp
+++ abc142_c.cpp
@@ -1,29 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool compare_by_second(pair<int, int> a, pair<int, int> b) {
-  if (a.second != b.second) {
-    return a.second < b.second;
-  } else {
-    return a.first < b.first;
-  }
-}
-
 int main() {
   int N;
   scanf("%d", &N);
 
-  vector<pair<int, int>> A(N);
+  vector<int> A(N, 0);
   for (int i = 0; i < N; i++) {
-    int order = 0;
-    scanf("%d", &order);
-    A.at(i) = make_pair(i + 1, order);
+    scanf("%d", &A.at(i));
   }
 
-  sort(A.begin(), A.end(), compare_by_second);
-
-  for (auto p : A) {
-    cout << p.first << " ";
-  }
+  vector<int> ans(N, 0);
+  for (int i = 0; i < N; i++) ans.at(A.at(i) - 1) = i + 1;
+  for (int i = 0; i < N; i++) printf("%d ", ans.at(i));
   return 0;
 }
