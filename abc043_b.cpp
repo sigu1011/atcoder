aba9738ebc8c6b9bf6bb4ce688ab52ddbd772adc
sigu <52273288+sigu1011@users.noreply.github.com>
2019-11-11 21:03:15 +0900

    より簡潔なコードに修正

--- abc043_b.cpp
+++ abc043_b.cpp
@@ -5,18 +5,13 @@ int main() {
   string s;
   cin >> s;
   string ans = "";
-  for (int i = 0; i < s.length(); i++) {
-    if (s.at(i) == '0') {
-      ans.push_back('0');
-    } else if (s.at(i) == '1') {
-      ans.push_back('1');
+  for (auto c : s) {
+    if (c == 'B') {
+      if (!ans.empty()) ans.erase(ans.end() - 1);
     } else {
-      if (!ans.empty()) {
-        ans.erase(ans.length() - 1);
-      }
+      ans += c;
     }
   }
-
   printf("%s", ans.c_str());
   return 0;
 }
