82f9932621ae5cdbc337fa90e718738ca8e7891b
sigu <52273288+sigu1011@users.noreply.github.com>
2019-12-03 18:46:08 +0900

    DFSを使用した解法に変更.こちらの方が実装がシンプルであり、時間計算量、空間計算量ともに少ない

--- abc146_d.cpp
+++ abc146_d.cpp
@@ -1,53 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Edge {
+  int to;
+  int id;
+  Edge(int t, int i) : to(t), id(i) {}
+};
+
+using Graph = vector<vector<Edge>>;
+vector<int> ans;
+
+void dfs(Graph &g, int v, int pre_color = -1, int parent = -1) {
+  int color = 1;
+  for (auto next_v : g.at(v)) {
+    int u = next_v.to;
+    int id = next_v.id;
+    if (u == parent) continue;
+    if (color == pre_color) color++;
+    ans.at(id) = color;
+    color++;
+    dfs(g, u, ans.at(id), v);
+  }
+}
+
 int main() {
   int N;
   cin >> N;
-  vector<vector<int>> graph(N);
-  vector<pair<int, int>> vp;
-  for (int i = 1; i < N; i++) {
+  vector<vector<Edge>> graph(N);
+  for (int i = 0; i < N - 1; i++) {
     int a, b;
     cin >> a >> b;
     a--;
     b--;
-    graph.at(a).push_back(b);
-    graph.at(b).push_back(a);
-    vp.emplace_back(a, b);
+    graph.at(a).push_back(Edge(b, i));
+    graph.at(b).push_back(Edge(a, i));
   }
 
-  // 各エッジの色
-  map<pair<int, int>, int> edges_color;
-  // 親を持つ場合、親に割り当てられた色
-  vector<int> assigned_color(N, 0);
+  ans.resize(N - 1);
+  dfs(graph, 0);
 
-  vector<int> seen(N, 0);
-  queue<int> que;
-
-  seen.at(0) = 1;
-  que.push(0);
-  while (!que.empty()) {
-    int v = que.front();
-    que.pop();
-    int cur = 1;
-    for (auto u : graph.at(v)) {
-      // 探索済みの頂点はスルー
-      if (seen.at(u)) continue;
-      // 親となる頂点に割り当てられた色は使用しない
-      if (cur == assigned_color.at(v)) cur++;
-      // 割り当てる色で無向グラフの辺の色を更新
-      assigned_color.at(u) = cur;
-      edges_color[make_pair(u, v)] = cur;
-      edges_color[make_pair(v, u)] = cur;
-      cur++;
-      seen.at(u) = 1;
-      que.push(u);
-    }
-  }
-
-  int K = 0;
-  for (int i = 0; i < N; i++) K = max(K, (int)graph.at(i).size());
-  cout << K << endl;
-  for (auto p : vp) cout << edges_color[p] << endl;
+  int mx = 0;
+  for (int i = 0; i < N; i++) mx = max(mx, (int)graph.at(i).size());
+  cout << mx << endl;
+  for (int i = 0; i < N - 1; i++) cout << ans.at(i) << endl;
   return 0;
 }
