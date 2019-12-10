f3dbd8e8039b842612ac348342f38815c95841f9
sigu <52273288+sigu1011@users.noreply.github.com>
2019-12-10 21:57:24 +0900

    別解.DFSを使用.再帰関数により二分木を構築し、ビット列に変換して検証する

--- abc147_c.cpp
+++ abc147_c.cpp
@@ -4,6 +4,32 @@ using namespace std;
 int A[20];
 int x[20][20];
 int y[20][20];
+int ans = 0;
+
+void update(string str, int n) {
+  // ビット列に変換
+  bitset<15> bits(str, 0, n, 'F', 'T');
+
+  bool is_consistent = true;
+  for (int i = 0; i < n; i++)
+    if (bits.test(i)) {
+      // 生成したビット列と各正直者の証言が整合しているか確認
+      for (int j = 0; j < A[i]; j++) {
+        if ((bits.test(x[i][j] - 1)) ^ y[i][j]) is_consistent = false;
+      }
+    }
+
+  if (is_consistent) ans = max(ans, (int)bits.count());
+}
+
+void dfs(string cur, int n, int depth) {
+  if (depth == n) {
+    update(cur, n);
+    return;
+  }
+  dfs("T" + cur, n, depth + 1);
+  dfs("F" + cur, n, depth + 1);
+}
 
 int main() {
   int N;
@@ -16,22 +42,8 @@ int main() {
     }
   }
 
-  int ans = 0;
-  // bit全探索
-  for (int bits = 0; bits < (1 << N); ++bits) {
-    bool is_consistent = true;
-
-    for (int i = 0; i < N; i++)
-      if ((bits >> i) & 1) {
-        // シミュレートしている状態と各正直者の証言が整合しているか確認
-        for (int j = 0; j < A[i]; j++) {
-          if ((bits >> (x[i][j] - 1)) & 1 ^ y[i][j]) is_consistent = false;
-        }
-      }
-
-    if (is_consistent) ans = max(ans, (int)bitset<15>(bits).count());
-  }
+  string start;
+  dfs(start, N, 0);
 
   cout << ans << endl;
-  return 0;
 }
