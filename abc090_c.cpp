edbabb8d31bf5ddedb0577d55ae58214d50b7669
sigu <52273288+sigu1011@users.noreply.github.com>
2020-01-10 00:16:22 +0900

    bug fix

--- abc090_c.cpp
+++ abc090_c.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-  int N, M;
+  long long N, M;
   cin >> N >> M;
-  if (N == 1 && M == 1) {
+  if (N == 1LL && M == 1LL) {
     printf("1");
-  } else if (N == 1 || M == 1) {
-    printf("%d", N * M - 2);
+  } else if (N == 1LL || M == 1LL) {
+    printf("%lld", N * M - 2LL);
   } else {
-    printf("%d", N * M - 2 * (N + M - 2));
+    printf("%lld", N * M - 2LL * (N + M - 2LL));
   }
   return 0;
 }
